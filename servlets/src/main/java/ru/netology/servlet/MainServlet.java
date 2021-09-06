aefa66cc37b6fcfba6c289f0eacf58e45b486c6e
Azathoth <vladislavutkin92@gmail.com>
2021-09-06 20:18:33 +0300

    - добавлен spring. Переписано на annotation-di

--- servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,8 +1,6 @@
 package ru.netology.servlet;
-
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.netology.controller.PostController;
-import ru.netology.repository.PostRepository;
-import ru.netology.service.PostService;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,22 +11,18 @@ public class MainServlet extends HttpServlet {
 
   @Override
   public void init() {
-    final var repository = new PostRepository();
-    final var service = new PostService(repository);
-    controller = new PostController(service);
+    final var context = new AnnotationConfigApplicationContext("ru.netology");
+    controller = (PostController) context.getBean("postController");
   }
-
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
     final var path = req.getRequestURI();
     try {
-      // primitive routing
       if (path.equals("/api/posts")) {
         controller.all(resp);
         return;
       }
       if (path.matches("/api/posts/\\d+")) {
-        // easy way
         final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
         controller.getById(id, resp);
         return;
